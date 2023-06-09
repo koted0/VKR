674245cd74ff04774616c1a47a053b7cc9fdd79f
kotedo <darkest.jerry@gmail.com>
2023-06-09 16:00:02 +0300

    v2

--- translate.py
+++ translate.py
@@ -1,5 +1,10 @@
 from deep_translator import GoogleTranslator
 
+
 def translate_text(text, source_lang='en', target_lang='ru'):
     translators = GoogleTranslator(source=source_lang, target=target_lang)
     return translators.translate(text)
+
+
+def get_translation_languages():
+    return GoogleTranslator().get_supported_languages(as_dict=True)
